fd7e3b984695e73183a69770e67309eacfd2085a
Roque Barbosa <roquecostajr@outlook.com>
2022-09-02 12:26:05 -0300

    :hammer: Refactor: Change action button location

--- src/Components/Home/Input.tsx
+++ src/Components/Home/Input.tsx
@@ -7,22 +7,10 @@ interface Iinput{
 
 export const Input: React.FC<Iinput> = ({value, onChangeFunction}) => {
   return (
-    <div className="flex h-12 w-full items-center justify-center
-      bg-[#C57B57]
-    ">
-      <input
+    <input
         type="number"
         value={value}
         onChange={(e) =>onChangeFunction(e.target.value)}
         className="focus: outline-none flex-[10] h-5/6 ml-[0.1rem] rounded-md pl-3" />
-      <button className="
-        flex flex-[2] 
-        bg-[#C57B57] 
-        items-center 
-        justify-center 
-        h-full">
-          <ArrowRight className="text-white"/>
-        </button>
-    </div>
   )
 }
--- src/Components/Home/StopsCalculator.tsx
+++ src/Components/Home/StopsCalculator.tsx
@@ -1,3 +1,4 @@
+import { ArrowRight } from "heroicons-react"
 import { useState } from "react"
 import { Input } from "./Input"
 
@@ -5,7 +6,18 @@ export const StopsCalculator: React.FC = () => {
   const [inputState, setInputState] = useState<number>(0)
   return (
     <div>
+      <div className="flex h-12 w-full items-center justify-center bg-[#C57B57]">
       <Input value={inputState} onChangeFunction={setInputState}/>
+      <button className="
+        flex flex-[2] 
+        bg-[#C57B57] 
+        items-center 
+        justify-center 
+        h-full">
+          <ArrowRight className="text-white"/>
+        </button>
+    </div>
+      
     </div>
   )
 }
