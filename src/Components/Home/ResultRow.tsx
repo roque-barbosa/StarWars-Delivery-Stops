c3843da12e9542fe2b457a7cf24e3446c91ff3e3
Roque Barbosa <roquecostajr@outlook.com>
2022-09-03 17:43:14 -0300

    :hammer: Refactor: Change stops count to show Unknow

--- src/Components/Home/ResultRow.tsx
+++ src/Components/Home/ResultRow.tsx
@@ -7,7 +7,7 @@ export const ResultRow: React.FC<IResultRow> = ({result}) => {
     <div className="flex w-full h-10 justify-between border-b-2 border-gray-300">
       <span className="flex flex-[8] items-center pl-4">{result.name}</span>
       <div className="flex flex-[2] justify-center items-center h-full">
-        <span className="flex bg-[#C57B57] h-8 w-8 rounded-full text-white justify-center items-center">
+        <span className="flex bg-[#C57B57] h-full w-full rounded-md p-1 text-white justify-center items-center">
           {result.stops}
         </span>
       </div>
